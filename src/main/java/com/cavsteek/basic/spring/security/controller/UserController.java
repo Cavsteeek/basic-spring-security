091f0193055a062c68fc8a7a7a31aee97a15e4f6
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2023-11-23 00:43:14 +0100

    10th commit

--- src/main/java/com/cavsteek/basic/spring/security/controller/UserController.java
+++ src/main/java/com/cavsteek/basic/spring/security/controller/UserController.java
@@ -1,4 +1,11 @@
 package com.cavsteek.basic.spring.security.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
 }
